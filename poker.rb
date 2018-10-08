4c1b1811516f7ebf924f4268328d6551e4e834ec
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-08 14:30:25 -0400

    T returns as value of 10

--- poker.rb
+++ poker.rb
@@ -2,7 +2,10 @@ class Card
     def initialize(number, suit)
         @card_name = "#{number}#{suit}"
         @card_value = "#{number}"
-
+        case number
+        when "T"
+            @card_value = "10"
+        end
     end
     attr_reader :card_value
     attr_reader :card_name
