1a48fbfb180c88c11ec95a23d7d8e352abe31f2b
Jordan Endicott <jordan.endicott823@gmail.com>
2018-11-15 11:46:32 -0500

    deals hands, retruns hands as strings

--- poker2.rb
+++ poker2.rb
@@ -1,10 +1,31 @@
 
 class Deck #establishes deck and cards
     VALUE = %i[two three four five six seven eight nine ten jack queen king ace]
-    SUITS = %i[heart club diamond spade]
+    SUITS = %i[hearts clubs diamonds spades]
+    
+    Card = Struct.new(:value, :suit) do 
+        def to_s #redefine to_s to return card names as string
+            "#{value.capitalize} of #{suit.capitalize}"
+        end
+        def ==(other) #redefine == to check for value instead of id
+            self.value == other.value &&
+              self.suit == other.suit
+          end
+    end
+    
+   deck = VALUE.flat_map {|val| SUITS.map {|suit| Card.new(val, suit)}}
+   shuffled = deck.shuffle
 
-    Card = Struct.new(:value, :suit)
-    VALUE.flat_map {|val| SUITS.map {|suit| Card.new(val, suit)}}
+    hand = []
+
+    5.times do
+        hand << shuffled.pop().to_s
+    end
+
+    puts hand
+   
+
+  
 end
 
 class Hand #deals hands, returns array with all valued info
