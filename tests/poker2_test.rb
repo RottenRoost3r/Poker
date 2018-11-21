79e70b4281c849c3264b41591eb84dbdd63652db
Jordan Endicott <jordan.endicott823@gmail.com>
2018-11-21 15:24:22 -0500

    fail two pair

--- tests/poker2_test.rb
+++ tests/poker2_test.rb
@@ -98,19 +98,6 @@ class Poker2 < Minitest::Test
         assert_equal(true, temp.hand.four_of_a_kind())
     end
 
-    # def test_two_pair_works
-    #     temp = Deck.new
-    #     temp_arr = []
-    #     temp_arr << ["9", "Spades"]
-    #     temp_arr << ["6", "Hearts"]
-    #     temp_arr << ["6", "Spades" ]
-    #     temp_arr << ["2", "Clubs"]
-    #     temp_arr << ["9", "Diamonds"]
-    #     temp.deal(temp_arr)
-    #     game = Hand.new
-    #     assert_equal(true, temp.hand.two_pair())
-    # end
-
     def test_blaaaah
         temp = Deck.new
         temp_arr = []
@@ -122,5 +109,18 @@ class Poker2 < Minitest::Test
         temp.deal(temp_arr)
         game = Hand.new
         assert_equal(true, temp.hand.full_house())
+    end
+
+    def test_two_pair_works
+        temp = Deck.new
+        temp_arr = []
+        temp_arr << ["9", "Spades"]
+        temp_arr << ["6", "Hearts"]
+        temp_arr << ["6", "Spades" ]
+        temp_arr << ["2", "Clubs"]
+        temp_arr << ["9", "Diamonds"]
+        temp.deal(temp_arr)
+        game = Hand.new
+        assert_equal(true, temp.hand.two_pair())
     end
 end
